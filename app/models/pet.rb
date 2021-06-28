0608b7aaedf2bc09c085baacbd0a90aa5f8ff60b
JSouza-git <j.otavio.ts@gmail.com>
2021-06-27 23:18:21 -0300

    Ajustando validação de model para a classe Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,7 @@
 class Pet < ApplicationRecord
+  validates :nome, presence: true
+  validates :raca, presence: true
+  validates :cor, presence: true
+  validates :idade, presence: true, numericality: {only_integer: true}, uniqueness: true
   belongs_to :cliente
 end
